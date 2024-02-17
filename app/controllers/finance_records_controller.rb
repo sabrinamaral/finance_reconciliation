ea52b6318258bc04996abcd56caa0d28ec58e0fa
sabrina amaral <sabrinamaraldev@gmail.com>
2024-02-17 20:32:36 -0300

    controller and routes

--- app/controllers/finance_records_controller.rb
+++ app/controllers/finance_records_controller.rb
@@ -2,47 +2,50 @@ class FinanceRecordsController < ApplicationController
   require 'csv'
 
   def new
-    @differences = []
+  end
+
+  def show
+    @csv_data1 = FinanceRecord.all
+    @csv_data2 = FinanceRecord2.all
   end
 
   def create
     file1 = params[:file1].path
     file2 = params[:file2].path
+    save_data_to_db(file1, file2)
 
-    csv1 = CSV.read(file1, headers: true, encoding: 'bom|utf-8')
-    csv2 = CSV.read(file2, headers: true, encoding: 'bom|utf-8')
+      redirect_to action: :show
+  end
 
-    @differences = compare_csvs(csv1, csv2)
-
-    render :new
+  def delete_all
+    FinanceRecord.delete_all
+    redirect_to root_path, notice: "All records have been deleted."
   end
 
   private
 
-  def compare_csvs(csv1, csv2)
-    differences = []
-
-    csv1.each do |row|
-      row2 = csv2.find { |r| r['id'] == row['id'] }
-
-      if row2
-        if row.to_h != row2.to_h
-          differences << row.to_h.merge(row2.to_h)
-          puts "Found difference: #{row.to_h.merge(row2.to_h)}"
-        end
-      else
-        differences << row.to_h
-        puts "Found row in csv1 not in csv2: #{row.to_h}"
-      end
-    end
-
-    csv2.each do |row|
-      unless csv1.find { |r| r['id'] == row['id'] }
-        differences << row.to_h
-        puts "Found row in csv2 not in csv1: #{row.to_h}"
-      end
-    end
-
-    differences
+  def save_data_to_db (file1, file2)
+    save_data_to_model(file1, FinanceRecord)
+    save_data_to_model(file2, FinanceRecord2)
   end
+
+  def save_data_to_model(file, model)
+    File.readlines(file).drop(1).each do |line|
+      input = line.strip.split(';')
+      amount = (input[3] && input[3].tr('R$', '').tr(',', '.').to_f) || 0.0
+
+      unless amount.zero? || amount.nil?
+        obj_data = {
+          date: input[0],
+          description: input[1],
+          amount: amount
+        }
+      end
+      record = model.new(obj_data)
+      unless record.save
+        puts "Failed to save record: #{record.errors.full_messages.join(", ")}"
+      end
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'finance_records#new'
+  get 'finance_records/show'
   post 'finance_records', to: 'finance_records#create'
+  delete 'finance_records', to: 'finance_records#delete_all'
 end
