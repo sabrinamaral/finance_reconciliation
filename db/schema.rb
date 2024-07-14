0cc3dc2455f55837695537dace691f96a6a61460
sabrina amaral <sabrinamaraldev@gmail.com>
2024-07-14 19:08:19 -0300

    chore: add user model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_03_124832) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_14_133657) do
   create_table "finance_record2s", force: :cascade do |t|
     t.date "date"
     t.text "description"
@@ -27,6 +27,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_03_124832) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "reconciled", default: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
