5a32a1a68f263d8dcc0231fd8a85649c53af1ca5
sabrina amaral <sabrinamaraldev@gmail.com>
2024-08-18 12:50:12 -0300

    Rename "type" column in cash_flows table to "transaction_type"

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_14_002033) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_18_125248) do
   create_table "cash_flows", force: :cascade do |t|
     t.date "date"
     t.string "description"
     t.decimal "amount"
-    t.integer "type"
+    t.integer "transaction_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
