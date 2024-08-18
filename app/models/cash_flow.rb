a92905715b393799224f86f2b285b58f68db542f
sabrina amaral <sabrinamaraldev@gmail.com>
2024-08-18 12:50:56 -0300

    Add CSV import functionality to CashFlow model

--- app/models/cash_flow.rb
+++ app/models/cash_flow.rb
@@ -1,2 +1,11 @@
 class CashFlow < ApplicationRecord
+  require 'csv'
+
+  def self.import_from_csv(file)
+    CSV.foreach(file.path, headers: true) do |row|
+      cash_flow_attributes = row.to_hash.transform_keys(&:underscore)
+      CashFlow.create!(cash_flow_attributes )
+    end
+  end
+
 end
