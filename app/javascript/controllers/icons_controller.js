572c894b6c7e94e12064c42cf9607031a32673d4
sabrina amaral <sabrinamaraldev@gmail.com>
2024-08-31 23:22:11 -0300

    Add a delete logic to icons_controller and config flash messages from the frontend side

--- app/javascript/controllers/icons_controller.js
+++ app/javascript/controllers/icons_controller.js
@@ -1,11 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["edit", "delete", "cancelEdit", "saveEdit"];
-
-  // connect() {
-  //   console.log("Icon controller connected");
-  // }
+  static targets = ["edit", "delete", "cancelEdit"];
 
   edit(event) {
     event.preventDefault();
@@ -24,9 +20,64 @@ export default class extends Controller {
     document.getElementById(`record-${recordId}`).style.display = "table-row";
     document.getElementById(`edit-form-${recordId}`).style.display = "none";
   }
-  // delete(event) {
-  //   event.preventDefault();
-  //   console.log("Delete icon clicked");
-  //   // Add your delete logic here
-  // }
+  delete(event) {
+    event.preventDefault();
+    const recordId = event.target.dataset.recordId;
+
+    const csrfToken = document
+      .querySelector('meta[name="csrf-token"]')
+      .getAttribute("content");
+
+    fetch(`/cash_flows/${recordId}`, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": csrfToken,
+        "Content-Type": "application/json",
+      },
+    })
+      .then(async (response) => {
+        const data = await response.json();
+        if (response.ok) {
+          document.getElementById(`record-${recordId}`).remove();
+          this.showFlashMessage("notice", data.message);
+        } else {
+          this.showFlashMessage("alert", data.error);
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        this.showFlashMessage(
+          "alert",
+          "Something went wrong while trying deleting this record."
+        );
+      });
+  }
+
+  showFlashMessage(type, message) {
+    // Clear existing flash messages
+    const flashMessagesContainer = document.querySelector(".flash-container");
+
+    if (flashMessagesContainer) {
+      flashMessagesContainer.innerHTML = "";
+    } else {
+      console.error("Flash messages container not found");
+      return;
+    }
+
+    // Create a new flash message
+    const flashContainer = document.createElement("div");
+    flashContainer.className = `alert alert-${
+      type === "notice" ? "info" : "danger"
+    } mt-4`;
+    flashContainer.setAttribute("role", "alert");
+    flashContainer.innerText = message;
+
+    // Append the new flash message
+    flashMessagesContainer.appendChild(flashContainer);
+
+    // Remove the flash message after 3 seconds
+    setTimeout(() => {
+      flashContainer.remove();
+    }, 3000);
+  }
 }
