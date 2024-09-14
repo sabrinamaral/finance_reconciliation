46d4fce1a9013fa60d6fc3d0da4d99c68d8fe33b
sabrina amaral <sabrinamaraldev@gmail.com>
2024-09-14 10:49:57 -0300

    add reset balance feature to controller and stimulus controller

--- app/javascript/controllers/balance_form_controller.js
+++ app/javascript/controllers/balance_form_controller.js
@@ -1,12 +1,11 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["form"];
+  static targets = ["form", "reset_btn"];
 
   submit(event) {
     event.preventDefault();
     const formData = new FormData(this.formTarget);
-    console.log(this.formTarget.method);
 
     fetch(this.formTarget.action, {
       method: this.formTarget.method,
@@ -21,6 +20,24 @@ export default class extends Controller {
         console.log("Form submitted successfully");
         this.formTarget.reset(); // reset the form
         window.location.href = "/cash_flows"; // Redirect to the cash flows path
+      }
+    });
+  }
+
+  reset(event) {
+    event.preventDefault();
+    fetch("/reset_balance", {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": document
+          .querySelector('meta[name="csrf-token"]')
+          .getAttribute("content"),
+        "Content-type": "application/jason",
+      },
+    }).then((response) => {
+      if (response.ok) {
+        console.log("Balance reset sucessfully");
+        window.location.href = "/cash_flows";
       }
     });
   }
