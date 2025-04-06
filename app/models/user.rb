afc680903ec14003db24f6aa960847feb37ab32d
Sabrina <sabrinamaraldev@gmail.com>
2025-04-06 16:19:33 -0300

    refactor authentication and user management: remove old controllers and implement Devise for user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
-# email:string
-# password_digest:string
-#
-#password:string virtual
-#password_confirmation:string virtual
-
 class User < ApplicationRecord
-  has_secure_password
-
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
