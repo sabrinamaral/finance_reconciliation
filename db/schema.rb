725e46c65a89af110a14c1036c23900cbe1c89ea
Sabrina <sabrinamaraldev@gmail.com>
2025-04-06 16:17:18 -0300

    add Devise authentication to users table with necessary fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_18_125248) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_06_011859) do
   create_table "cash_flows", force: :cascade do |t|
     t.date "date"
     t.string "description"
@@ -39,10 +39,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_18_125248) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
-    t.string "password_digest"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "remember_created_at"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
 end
