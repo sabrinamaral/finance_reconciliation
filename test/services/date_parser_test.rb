fe895aae4cbe5d71e6d5b1bf23c11858676f082e
Sabrina <sabrinamaraldev@gmail.com>
2025-04-30 14:19:31 -0300

    Implement data saver and date parser services, separate it from the reconciliation controller and add tests for both services

---
+++ test/services/date_parser_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class DateParserTest < ActiveSupport::TestCase
+  test "should parse valid date in DD/MM/YYYY format" do
+    assert_equal Date.new(2025, 4, 7), DateParser.parse("07/04/2025")
+  end
+
+  test "should parse valid date in YYYY-MM-DD format" do
+    assert_equal Date.new(2025, 4, 7), DateParser.parse("2025-04-07")
+  end
+  test "should parse valid date in DD-MM-YYYY format" do
+    assert_equal Date.new(2025, 4, 7), DateParser.parse("07-04-2025")
+  end
+
+end
