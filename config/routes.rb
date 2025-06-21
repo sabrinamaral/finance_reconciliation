e0d92a036eb3cb69aa26bdf2e91ba49ff6fd374e
Sabrina <sabrinamaraldev@gmail.com>
2025-06-21 14:54:31 -0300

    Add 'add_transaction' route to cash_flows resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :cash_flows, only: [:create, :index, :update, :destroy] do
     collection do
+      post "add_transaction"
       delete 'delete_all'
     end
   end
