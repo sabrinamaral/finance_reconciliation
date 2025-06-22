83c9110efe72e0fbd16ae9e790e664ef8d4da566
Sabrina <sabrinamaraldev@gmail.com>
2025-06-21 23:38:09 -0300

    Refactor highlight_rows_controller.js for consistent quotation marks and formatting

--- app/javascript/controllers/highlight_rows_controller.js
+++ app/javascript/controllers/highlight_rows_controller.js
@@ -1,17 +1,14 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = [ "row" ]
-  connect() {
-
-  }
+  static targets = ["row"];
+  connect() {}
 
   changeColor(event) {
     let row = event.currentTarget;
-    let cells = row.getElementsByTagName('td');
+    let cells = row.getElementsByTagName("td");
     for (let i = 0; i < cells.length; i++) {
-      cells[i].classList.toggle('highlight');
+      cells[i].classList.toggle("highlight");
     }
   }
-
 }
