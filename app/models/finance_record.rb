ea8153cada4821aedf34e304685d37be3bb919d6
Sabrina <sabrinamaraldev@gmail.com>
2025-07-19 18:23:11 -0300

    Add user association and current user scope to cash flow and finance record models

--- app/models/finance_record.rb
+++ app/models/finance_record.rb
@@ -1,4 +1,7 @@
 class FinanceRecord < ApplicationRecord
+  belongs_to :user
+  scope :for_current_user, -> { where(user_id: Current.user.id) if Current.user }
+
   validates :date, presence: true
   validates :amount, presence: true, numericality: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :cash_flows, dependent: :destroy
+  has_many :finance_records, dependent: :destroy
+  has_many :finance_record2s, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
