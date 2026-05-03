2ff393e29721821511f9a8f427cbb341e340df4a
Sabrina <sabrinamaraldev@gmail.com>
2026-05-03 09:19:20 -0300

    add resend mailer set up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 require "active_support/core_ext/integer/time"
+require "resend"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -56,14 +57,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 465,
-    tls: true,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
-    authentication: :plain
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  address: 'smtp.resend.com',
+  port: 587,
+  user_name: 'resend',
+  password: ENV['RESEND_API_KEY'],
+  authentication: :plain,
+  enable_starttls_auto: true
 }
 config.action_mailer.default_url_options = { host: 'financereconciliation-financeapp.up.railway.app', protocol: 'https' }
 
